ff5e6d1e7c620fc69780a5676b03f41e0c117625
rodrigobutta <rbutta@gmail.com>
2020-12-03 11:04:39 -0300

    Add Prettier (and fix code about it)

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,12 +1,10 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-import Routes from './routes';
+import Routes from "./routes";
 
 const App: React.FC = () => {
-  return (
-    <Routes/>
-  );
-}
+  return <Routes />;
+};
 
 export default App;
--- web/src/services/api.ts
+++ web/src/services/api.ts
@@ -1,9 +1,8 @@
-import axios from 'axios';
-import { API_HOST, API_PORT } from '../config/config';
-
+import axios from "axios";
+import { API_HOST, API_PORT } from "../config/config";
 
 const api = axios.create({
-    baseURL: `http://${API_HOST}:${API_PORT}`
+  baseURL: `http://${API_HOST}:${API_PORT}`,
 });
 
 export default api;
